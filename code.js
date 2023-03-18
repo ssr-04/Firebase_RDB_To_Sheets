83ab2b1013b393512fba6bbdd40dbf2c35cacf3d
ssr-04 <97006404+ssr-04@users.noreply.github.com>
2023-03-18 20:47:22 +0530

    initial commit

---
+++ code.js
@@ -0,0 +1,35 @@
+function update() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheets = ss.getSheets();
+  var sheet = ss.getActiveSheet();
+ 
+  var firebaseUrl = "Your firebase Realtime database URL";
+
+  var base = FirebaseApp.getDatabaseByUrl(firebaseUrl);
+  var dataSet = base.getData();
+
+  var values = [];
+  var datetime = new Date().toLocaleString();
+  values.push(datetime);
+  
+  for(var key in dataSet) {
+  var value = dataSet[key];
+  values.push(value);
+}
+sheet.appendRow(values);
+
+}
+
+function doGet(e) {
+  var x = HtmlService.createTemplateFromFile("index.html");
+  var y = x.evaluate();
+  var z = y.setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+  return z;
+}
+
+function getSheetData()  { 
+  var a= SpreadsheetApp.getActiveSpreadsheet();
+  var b = a.getSheetByName('Sheet1'); 
+  var c = b.getDataRange();
+  return c.getValues().reverse();  
+}
